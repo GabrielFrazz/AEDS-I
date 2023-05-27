6c59fe40beecc37716e2fd85ca2ecf72328227a6
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2023-05-27 17:51:07 -0300

    problem 12 done

---
+++ Recursion_problems/Exercicio12/Invert.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+char* invertsString(int size, char str[], int i);
+void printString(char str[],int size);
+
+int main(){
+    char str[7] = "Hello!T";
+    printString(str,6);
+    invertsString(6, str, 0);
+    printString(str,6);
+}
+
+char* invertsString(int size, char str[], int i){
+    if(i>=size){
+        return str;
+    }else{
+        char temp = str[size-1];
+        str[size-1]= str[i];
+        str[i] = temp;
+        return invertsString(size-1,str,i+1);
+    }
+}
+
+void printString(char str[], int size){
+    for(int i = 0; i < size; i++){
+        printf("%c",str[i]);
+    }
+    printf("\n");
+}
