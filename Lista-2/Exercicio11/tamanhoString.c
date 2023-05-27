199f7b373dc5506bffccb41871b7717b96b4673e
Carlos Gabriel de Oliveira Frazao <carlos.gofrazao@gmail.com>
2023-05-27 16:44:07 -0300

    Problem 11 done

---
+++ Lista-2/Exercicio11/tamanhoString.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int tamanhoString(char string[], int cont){
+    if(string[cont] == '\0'){
+        return cont-1;
+    }
+    return tamanhoString(string,cont+1);
+}
+
+int main(){
+    char string[101];
+    printf("digite a sua string: ");
+    fgets(string, 100, stdin);
+
+    printf("o tamanho da string e: %d",tamanhoString(string,0));
+}
